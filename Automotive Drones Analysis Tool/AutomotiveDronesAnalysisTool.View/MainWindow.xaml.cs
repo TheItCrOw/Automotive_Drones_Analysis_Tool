85d899a712443e4cacc7a9f76a9b2bb9ac8aef4b
Kevin Bönisch <k.boenisch@outlook.com>
2020-05-05 22:26:15 +0200

    Reading XMP and Exif file data

--- Automotive Drones Analysis Tool/AutomotiveDronesAnalysisTool.View/MainWindow.xaml.cs
+++ Automotive Drones Analysis Tool/AutomotiveDronesAnalysisTool.View/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MetadataExtractor;
+using MetadataExtractor.Formats.Xmp;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using XmpCore;
 
 namespace AutomotiveDronesAnalysisTool.View
 {
@@ -23,6 +28,21 @@ namespace AutomotiveDronesAnalysisTool.View
         public MainWindow()
         {
             InitializeComponent();
+
+            // Testing
+            // Get the EXIF metadata
+            var list = new StringBuilder();
+            var directories = ImageMetadataReader.ReadMetadata("E:\\WPF Projects\\Automotive Drones Analysis Tool\\Daten_automatisches_Fahren\\Situation_1_Auswahl\\Situation_1_nah.JPG");
+            foreach (var directory in directories)
+                foreach (var tag in directory.Tags)
+                    list.AppendLine($"{directory.Name} - {tag.Name} = {tag.Description}");
+
+            // Get the XMP meta data
+            var xmpProps = directories.OfType<XmpDirectory>().FirstOrDefault().GetXmpProperties();
+            foreach (var pair in xmpProps)
+                list.AppendLine($"{pair.Key} - {pair.Value}");
+
+            //File.WriteAllText("E:\\WPF Projects\\Automotive Drones Analysis Tool\\Daten_automatisches_Fahren\\Situation_1_Auswahl\\Situation_1_nah_metadata.txt", list.ToString());
         }
     }
 }
