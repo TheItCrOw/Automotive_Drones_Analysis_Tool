cc9dd24b60adce84e07475bf05be3096e1452af4
Kevin Bönisch <k.boenisch@outlook.com>
2020-05-05 22:27:05 +0200

    Merge fixed

---
+++ Automotive Drones Analysis Tool/AutomotiveDronesAnalysisTool.Model/Bases/ModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomotiveDronesAnalysisTool.Model.Bases
+{
+    /// <summary>
+    /// Base class for every model
+    /// </summary>
+    public class ModelBase
+    {
+        public Guid Id { get; set; }
+
+    }
+}
