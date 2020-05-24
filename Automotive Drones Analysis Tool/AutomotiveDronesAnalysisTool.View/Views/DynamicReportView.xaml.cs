341ac589b6323a71c49bd3550c2791e68b4e9982
unknown <keboen@web.de>
2020-05-24 18:35:03 +0200

    Add buttons on canvas at the right spot

--- Automotive Drones Analysis Tool/AutomotiveDronesAnalysisTool.View/Views/DynamicReportView.xaml.cs
+++ Automotive Drones Analysis Tool/AutomotiveDronesAnalysisTool.View/Views/DynamicReportView.xaml.cs
@@ -22,7 +22,8 @@ namespace AutomotiveDronesAnalysisTool.View.Views
     public partial class DynamicReportView : UserControl
     {
         AnalysableImageViewModel _viewModel;
-        DetectedItemViewModel _testObject;
+        List<DetectedItemViewModel> _detectedObjects;
+
         public DynamicReportView()
         {
             InitializeComponent();
@@ -37,44 +38,29 @@ namespace AutomotiveDronesAnalysisTool.View.Views
         private void DynamicReportView_InitializedViewModel(AnalysableImageViewModel viewModel)
         {
             _viewModel = viewModel;
-            _testObject = _viewModel.DetectedObjects.FirstOrDefault();
+            _detectedObjects = _viewModel.DetectedObjects.ToList();
         }
 
         private void ViewModelImage_Canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
             ViewModelImage_Canvas.Children.Clear();
-            var test = ViewModel_Image.ActualWidth;
-            var testPoint = ViewModelImage_Canvas.TransformToAncestor(Main_Grid).Transform(new Point(0, 0));
-            var test3 = ViewModel_Image.TranslatePoint(new Point(0,0), ViewModelImage_Canvas);
-            var test2 = ViewModel_Image.ActualHeight;
-
-            double widthRatio = _viewModel.Image.Width / (ViewModelImage_Canvas.ActualWidth);
-            double heightRatio = _viewModel.Image.Width / (ViewModelImage_Canvas.ActualHeight);
-
-            var test35 = ViewModelImage_Canvas.Width / ViewModelImage_Canvas.Height; // 1.5 DAS IST WICHTIG
-            var test355 = ViewModelImage_Canvas.ActualWidth / ViewModelImage_Canvas.Width;
-
-            var button = new Button()
+            foreach (var item in _detectedObjects)
             {
-                Width = _testObject.Width / widthRatio * 1.33f,
-                Height = _testObject.Height / heightRatio * 2f
-            };
-            Canvas.SetLeft(button, _testObject.X / widthRatio * 1.33f);
-            Canvas.SetTop(button, _testObject.Y / heightRatio * 2);
+                // Important to get the PixelWidth and Height instead of regular height/widht!
+                double widthRatio = _viewModel.Image.PixelWidth / (ViewModelImage_Canvas.ActualWidth);
+                double heightRatio = _viewModel.Image.PixelHeight / (ViewModelImage_Canvas.ActualHeight);
 
-            ViewModelImage_Canvas.Children.Add(button);
+                var button = new Button()
+                {
+                    Width = item.Width / widthRatio, // 1.33 und 2
+                    Height = item.Height / heightRatio
+                };
 
-            var _startPoint = e.GetPosition(ViewModelImage_Canvas);
+                Canvas.SetLeft(button, item.X / widthRatio);
+                Canvas.SetTop(button, item.Y / heightRatio);
 
-            //var object0 = _viewModel.DetectedObjects.FirstOrDefault();
-            //var button = new Button()
-            //{
-            //    Width = object0.Width,
-            //    Height = object0.Height
-            //};
-            //Canvas.SetLeft(button, object0.X);
-            //Canvas.SetTop(button, object0.Y);
-            //ViewModelImage_Canvas.Children.Add(button);
+                ViewModelImage_Canvas.Children.Add(button);
+            }
         }
     }
 }
