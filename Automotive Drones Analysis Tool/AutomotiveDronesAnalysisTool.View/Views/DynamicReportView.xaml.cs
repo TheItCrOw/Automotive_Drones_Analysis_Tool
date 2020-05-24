f8f0312edb6b360be2a5f04283a1e4514d062a86
Kevin Bönisch <k.boenisch@outlook.com>
2020-05-24 21:44:49 +0200

    Handle window resizing

--- Automotive Drones Analysis Tool/AutomotiveDronesAnalysisTool.View/Views/DynamicReportView.xaml.cs
+++ Automotive Drones Analysis Tool/AutomotiveDronesAnalysisTool.View/Views/DynamicReportView.xaml.cs
@@ -21,6 +21,8 @@ namespace AutomotiveDronesAnalysisTool.View.Views
     /// </summary>
     public partial class DynamicReportView : UserControl
     {
+        System.Timers.Timer timer; //Declare it as a class member, not a local field, so it won't get GC'ed. 
+
         AnalysableImageViewModel _viewModel;
         List<DetectedItemViewModel> _detectedObjects;
 
@@ -41,7 +43,7 @@ namespace AutomotiveDronesAnalysisTool.View.Views
             _detectedObjects = _viewModel.DetectedObjects.ToList();
         }
 
-        private void ViewModelImage_Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        private void ViewModelImage_Canvas_Loaded(object sender, RoutedEventArgs e)
         {
             ViewModelImage_Canvas.Children.Clear();
             foreach (var item in _detectedObjects)
@@ -61,6 +63,47 @@ namespace AutomotiveDronesAnalysisTool.View.Views
 
                 ViewModelImage_Canvas.Children.Add(button);
             }
+            HandleWindowResize();
+        }
+
+        /// <summary>
+        /// Reposition the buttons upon resizing the window.
+        /// </summary>
+        private void HandleWindowResize()
+        {
+            timer = new System.Timers.Timer(100);
+            timer.AutoReset = false; //the Elapsed event should be one-shot
+            timer.Elapsed += (o, e) =>
+            {
+                //Since this is running on a background thread you need to marshal it back to the UI thread.
+                Dispatcher.BeginInvoke(new Action(() => {
+                    ViewModelImage_Canvas.Children.Clear();
+                    foreach (var item in _detectedObjects)
+                    {
+                        // Important to get the PixelWidth and Height instead of regular height/widht!
+                        double widthRatio = _viewModel.Image.PixelWidth / (ViewModelImage_Canvas.ActualWidth);
+                        double heightRatio = _viewModel.Image.PixelHeight / (ViewModelImage_Canvas.ActualHeight);
+
+                        var button = new Button()
+                        {
+                            Width = item.Width / widthRatio, // 1.33 und 2
+                            Height = item.Height / heightRatio
+                        };
+
+                        Canvas.SetLeft(button, item.X / widthRatio);
+                        Canvas.SetTop(button, item.Y / heightRatio);
+
+                        ViewModelImage_Canvas.Children.Add(button);
+                    }
+                }));
+            };
+
+            this.SizeChanged += (o, e) =>
+            {
+                //restart the time if user is still manipulating the window             
+                timer.Stop();
+                timer.Start();
+            };
         }
     }
 }
